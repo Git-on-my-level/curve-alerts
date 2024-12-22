5df0df582ce67b97927092234602ff6cf589b7c1
David Zhang <david.d.zhang@gmail.com>
2024-12-22 12:37:08 -0800

    Get CDK deployment working and running on AWS (#2)

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,24 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as events from 'aws-cdk-lib/aws-events';
-import * as targets from 'aws-cdk-lib/aws-events-targets';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import { Construct } from "constructs";
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Create Lambda function
-    const botFunction = new lambda.Function(this, 'BotFunction', {
+    const botFunction = new lambda.Function(this, "BotFunction", {
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'index.cjs.handler',
-      code: lambda.Code.fromAsset('dist'),
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("dist"),
       timeout: cdk.Duration.minutes(5),
       memorySize: 256,
     });
 
     // Create EventBridge rule to trigger Lambda every 15 minutes
-    const rule = new events.Rule(this, 'ScheduleRule', {
+    const rule = new events.Rule(this, "ScheduleRule", {
       schedule: events.Schedule.rate(cdk.Duration.minutes(15)),
     });
 
